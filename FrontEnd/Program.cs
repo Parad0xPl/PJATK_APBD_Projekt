ef781969a3d55fc442fa72de2723ac555f8e4a86
Parad0xPl <paradxpl@gmail.com>
2022-06-01 19:37:38 +0200

    Add auto refresh on get details

--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -1,14 +1,29 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FrontEnd;
+using FrontEnd.Utils;
+using Blazored.LocalStorage;
 using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var defaultHttpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)};
-builder.Services.AddScoped(sp => defaultHttpClient);
+var defaultHttpClient = new ApiClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)};
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped(sp =>
+{
+    var syncLocalStorageService = sp.GetService<ISyncLocalStorageService>();
+    defaultHttpClient.SetLocalStorage(syncLocalStorageService);
+
+    var auth = syncLocalStorageService.GetItemAsString("Auth-Token");
+    if (auth != null)
+    {
+        defaultHttpClient.SetAuthorization(auth);
+    }
+
+    return defaultHttpClient;
+});
 builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
 await builder.Build().RunAsync();
 
---
+++ FrontEnd/Utils/ApiClient.cs
@@ -0,0 +1,82 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Blazored.LocalStorage;
+using Shared.DTO;
+
+namespace FrontEnd.Utils;
+
+public class ApiClient : HttpClient
+{
+    private ISyncLocalStorageService? _localStorage;
+
+    public void SetAuthorization(string token)
+    {
+        var authName =
+            "Authorization";
+        var hasAuth = this
+            .DefaultRequestHeaders
+            .Contains(authName);
+        if (hasAuth)
+        {
+            this
+                .DefaultRequestHeaders
+                .Remove(authName);
+        }
+        this
+            .DefaultRequestHeaders
+            .Add(authName, "Bearer " + token);
+        
+        _localStorage!.SetItemAsString("Auth-Token", token);
+    }
+
+    public void SetLocalStorage(ISyncLocalStorageService syncLocalStorageService)
+    {
+        _localStorage = syncLocalStorageService;
+    }
+
+    public async Task<bool> RefreshToken()
+    {
+        var response = await GetAsync("api/refresh");
+        if (!response.IsSuccessStatusCode)
+        {
+            return false;
+        }
+
+        var readFromJsonAsync = await response.Content.ReadFromJsonAsync<LoginResponseDTO>();
+        if (readFromJsonAsync == null)
+        {
+            return false;
+        }
+        //TODO go to login if can't refresh
+        SetAuthorization(readFromJsonAsync.JWTToken);
+        return true;
+    }
+
+    public async Task<TickerDetailsDTO?> GetDetails(string name)
+    {
+        using var response = await this.GetAsync($"/api/Stock/{name}");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                var refreshToken = await RefreshToken();
+                if (!refreshToken)
+                {
+                    return null;
+                }
+
+                return await GetDetails(name);
+            }
+            else
+            {
+                
+                return null;
+            }
+        }
+
+        var details = await response.Content.ReadFromJsonAsync<TickerDetailsDTO>();
+        return details;
+
+    }
+}
