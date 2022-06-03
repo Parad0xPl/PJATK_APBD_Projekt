7545460fcc08bfcf2b9481a001ac6f63b90e8a30
Parad0xPl <paradxpl@gmail.com>
2022-06-04 00:53:20 +0200

    - Add dynamic search complete

---
+++ Server/Migrations/20220603162658_AddCachedImages.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    public partial class AddCachedImages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cached",
+                columns: table => new
+                {
+                    Url = table.Column<string>(type: "TEXT", maxLength: 320, nullable: false),
+                    Data = table.Column<byte[]>(type: "BLOB", maxLength: 4194304, nullable: false),
+                    Type = table.Column<string>(type: "TEXT", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cached", x => x.Url);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cached");
+        }
+    }
+}
